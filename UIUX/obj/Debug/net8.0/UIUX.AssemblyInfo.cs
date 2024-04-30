6073f6e0330bb7813816d4544462b2c1efffd821
ibroadwell <115008310+ibroadwell@users.noreply.github.com>
2024-04-30 14:40:24 +0100

    update

--- UIUX/obj/Debug/net8.0/UIUX.AssemblyInfo.cs
+++ UIUX/obj/Debug/net8.0/UIUX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UIUX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+240304e5c0a05d80a26736990c6ae0ba56876b2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ac9f64511f23f01ef9dd5c42da3454b97fcf52e")]
 [assembly: System.Reflection.AssemblyProductAttribute("UIUX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UIUX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
